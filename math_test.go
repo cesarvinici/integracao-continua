5eae2ab61c9b2ae643b0e16062018b7d24022bd9
Cesar Vinicius <cesarvinici@gmail.com>
2023-12-21 15:31:30 +0000

    Add subtracao function and corresponding test

--- math_test.go
+++ math_test.go
@@ -9,3 +9,11 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Soma incorreta, resultado: %d, esperado: %d.", total, 30)
 	}
 }
+
+func TestSubtracao(t *testing.T) {
+	total := subtracao(15, 15)
+
+	if total != 0 {
+		t.Errorf("Subtracao incorreta, resultado: %d, esperado: %d.", total, 0)
+	}
+}
